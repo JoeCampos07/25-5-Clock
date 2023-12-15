6a40b65e924fd21cb3eb0eb48a390e08a0f188c2
JoeCampos <joe.campos07@gmail.com>
2023-12-15 14:26:48 -0600

    correction in the base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://joecampos07.github.io/25-5Clock/",
+  base: "https://joecampos07.github.io/25-5-Clock/",
   plugins: [react()],
 })
