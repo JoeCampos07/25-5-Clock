87a28b2ac5311b66103a8d9851a03472608e9093
JoeCampos <joe.campos07@gmail.com>
2023-12-15 14:19:35 -0600

    Vite deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/25-5Clock/",
+  base: "https://joecampos07.github.io/25-5Clock/",
   plugins: [react()],
 })
